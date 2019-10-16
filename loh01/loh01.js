76cd04038be484bb16df4ff5d7d71590cfbd22f0
gonarb <Tanck1987@yandex.ru>
2019-10-16 19:50:23 +0900

    chetvetii

--- loh01/loh01.js
+++ loh01/loh01.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -10,15 +10,35 @@ let appData = {
     savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a2 = prompt("Во сколько обойдется?", ''),
-    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    a4 = prompt("Во сколько обойдется?", '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
 
-alert("Vash dnevnoi budget: "+appData.budget/30);
+for  (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+
+    if ((typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null 
+    && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        
+    };
+    
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Vash dnevnoi budget: " +appData.appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Minimalnii uriven dostatka!")
+} else if (appData.moneyPerDay >100 && appData.moneyPerDay < 2000) {
+    console.log("Srednii uriven dostatka!");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Visokii uriven dostatka!");
+} else {
+    console.log("Proizoshla oshibka!");
+}
 
 
     
