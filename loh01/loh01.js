fa96bd4f8307858e65975a5f43d2ac3be8130090
gonarb <Tanck1987@yandex.ru>
2019-10-16 19:23:19 +0900

    tretii commit

---
+++ loh01/loh01.js
@@ -0,0 +1,24 @@
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let appData = {
+    timeData: time,
+    budget: money,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a2 = prompt("Во сколько обойдется?", ''),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+    a4 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert("Vash dnevnoi budget: "+appData.budget/30);
+
+
+    
